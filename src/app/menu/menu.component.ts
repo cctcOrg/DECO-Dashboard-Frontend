e0d4cf158d7079eb7df594897fa9b34e76f10eb9
Vincy <vnxichow@gmail.com>
2017-07-24 13:18:39 -0700

    changed sidebar to materialize and angular

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MenuComponent implements OnInit {
-  links = ["Settings", "Case 1", "Case 2"];
+  links = ["Settings", "Account Info", "Case 1", "Case 2"];
 
   constructor() { }
 
