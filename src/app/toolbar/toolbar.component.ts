a0fb23e3908814f089968b1c86cdf4936422d7bc
Nick <nick.ryan002@gmail.com>
2017-07-31 13:23:05 -0700

    Fixed breadcrumb errors

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -15,42 +15,31 @@ export class ToolbarComponent implements OnInit {
   isFile = true;
   
   constructor() { }
-
-  @Output() caseEvent = new EventEmitter();
-  @Output() deviceEvent = new EventEmitter();
-  @Output() dmEvent = new EventEmitter();
-  @Output() imageEvent = new EventEmitter();
-  @Output() fileEvent = new EventEmitter();
   
   ngOnInit() {
   }
 
   displayCaseBreadcrumb() {
-    this.caseEvent.emit(null);
     this.isDevice = this.isDM = this.isImage = this.isFile = false;
     this.isCase = true;
   }
   
   displayDeviceBreadcrumb() {
-    this.deviceEvent.emit(null);
     this.isDM = this.isImage = this.isFile = false;
     this.isCase = this.isDevice = true;
   }
 
   displayDMBreadcrumb() {
-    this.dmEvent.emit(null);
     this.isImage = this.isFile = false;
     this.isCase = this.isDevice = this.isDM = true;
   }
 
   displayImageBreadcrumb() {
-    this.imageEvent.emit(null);
     this.isFile = false;
     this.isCase = this.isDevice = this.isDM = this.isImage = true;
   }
 
   displayFileBreadcrumb() {
-    this.fileEvent.emit(null);
     this.isCase = this.isDevice = this.isDM = this.isImage = this.isFile = true;
   }
 }
