1b62d018fa107a463c10ba381c5112a3fcd06b85
Duc Dao <ducdao11@gmail.com>
2017-11-09 14:45:11 -0800

    Changed url that referred to the back end. Added header picture in sidenav.

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -3,8 +3,8 @@ import {Http, RequestOptions, Headers} from '@angular/http';
 
 @Injectable()
 export class ServerService {
-  // Local Fedora server with 5000 port URL
-  url = 'http://129.65.100.50:5000';
+  // Local Fedora server with 5000 port URL, old IP 129.65.100.50
+  url = 'http://172.30.86.133:5000';
 
   // EC2 Server URL
   // url = 'http://ec2-34-212-218-147.us-west-2.compute.amazonaws.com:80';
